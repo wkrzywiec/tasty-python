73894c453e4102ec06ae111e81ec02b382611f04
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-03-08 12:23:00 +0100

    add logs to test

--- tests/test_tasty_client.py
+++ tests/test_tasty_client.py
@@ -5,8 +5,13 @@ class TestTastyClient(unittest.TestCase):
 
     def test_find_pizza_margherita_recipes(self):
         recipes = tasty.find_reciepes('pizza margherita')
-        
         self.assertGreaterEqual(len(recipes), 6)
+        
+        for recipe in recipes:
+            print(recipe.key)
+            print(recipe.full_name)
+            print(recipe.url)
+            print()
     
 if __name__ == '__main__':
     unittest.main()
