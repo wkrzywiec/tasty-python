2f3621fd3fadce72a9cd595e81eedd5cab29a17d
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-03-08 15:01:19 +0100

    get recipe title

--- tasty_python/tasty.py
+++ tasty_python/tasty.py
@@ -5,8 +5,8 @@ baseUrl = 'https://tasty.co/'
 
 class Recipe:
     
-    def __init__(self, name, ingredients, preparation, url):
-        self.name = name
+    def __init__(self, title, ingredients, preparation, url):
+        self.title = title
         self.ingredients = ingredients
         self.preparation = preparation
         self.url = url
@@ -22,8 +22,8 @@ def find_reciepes(query):
     searchUrl = 'search?q='
     query = __encode_space(query)
     result_page = requests.get(baseUrl + searchUrl + query)
-    
     soup = BeautifulSoup(result_page.text, 'html.parser')
+
     page_items_list = soup.find_all(
         attrs={"data-vars-object-type": "recipe", "class": "feed-item analyt-unit-tap"})
     recipes_list = []
@@ -36,6 +36,15 @@ def find_reciepes(query):
         )
         recipes_list.append(recipe)
     return recipes_list
+
+def get_recipe(url):
+    result_page = requests.get(url)
+    soup = BeautifulSoup(result_page.text, 'html.parser')
+
+    title = soup.find('h1').string
+    ingredients = []
+    preparation = ''
+    return Recipe(title, ingredients, preparation, url)
 
 def __encode_space(query):
     return query.replace(' ', '+')
--- tests/test_tasty_client.py
+++ tests/test_tasty_client.py
@@ -5,13 +5,19 @@ class TestTastyClient(unittest.TestCase):
 
     def test_find_pizza_margherita_recipes(self):
         recipes = tasty.find_reciepes('pizza margherita')
-        self.assertGreaterEqual(len(recipes), 6)
-        
+
         for recipe in recipes:
             print(recipe.key)
             print(recipe.full_name)
             print(recipe.url)
             print()
+        self.assertGreaterEqual(len(recipes), 6)
+        
+    def test_get_pizza_margherita_by_mario_batali_recipe(self):
+        recipe = tasty.get_recipe('https://tasty.co/recipe/pizza-margherita-by-mario-batali')
+
+        print(recipe.title)
+        self.assertEqual(recipe.title, 'Pizza Margherita by Mario Batali')
     
 if __name__ == '__main__':
     unittest.main()
