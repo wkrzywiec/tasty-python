e1b29775c9c086577714e55970e5bc81c961051a
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-03-11 06:57:50 +0100

    get recipe method

--- tasty_python/tasty_client.py
+++ tasty_python/tasty_client.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 baseUrl = 'https://tasty.co/'
 
-class Recipe:
-    
+class Recipe: 
     def __init__(self, title, ingredients, preparation, url):
         self.title = title
         self.ingredients = ingredients
@@ -12,7 +12,6 @@ class Recipe:
         self.url = url
 
 class RecipeSearchResult:
-
     def __init__(self, key, full_name, url):
         self.key = key
         self.full_name = full_name
@@ -42,9 +41,42 @@ def get_recipe(url):
     soup = BeautifulSoup(result_page.text, 'html.parser')
 
     title = soup.find('h1').string
-    ingredients = []
+    ingredients = __get_ingredients(soup)
     preparation = ''
     return Recipe(title, ingredients, preparation, url)
 
 def __encode_space(query):
     return query.replace(' ', '+')
+
+def  __get_ingredients(soup):
+    ingredients = []
+    points_list = soup.find_all("li", class_="ingredient")
+    for point_tag in points_list:
+        ingredient = __extract_formatted_ingredient(point_tag)
+        ingredients.append(ingredient)
+    return ingredients
+
+def __extract_formatted_ingredient(point_tag):
+    ingredient = ''
+    
+    for child in point_tag:
+        s = str(child)
+        
+        if s.startswith('<span'):
+            s = __remove_span_tag(s)
+        
+        ingredient_part = s.split()
+        for part in ingredient_part:
+            ingredient = ingredient + part + ' '
+    ingredient = re.sub(' , ', ', ', ingredient)
+    ingredient = ingredient.strip()
+    return ingredient
+
+def __remove_span_tag(s):
+    s = s.replace('<!-- -->', '')
+    s = s.replace('</span>', '')
+    span_tag = re.search('<(.*?)>', s).group(0)
+    s = re.sub(span_tag, ' ', s)
+    return s
+
+
--- tests/test_tasty_client.py
+++ tests/test_tasty_client.py
@@ -1,23 +1,31 @@
 import unittest
-import tasty_python.tasty as tasty
+import tasty_python.tasty_client as tasty
 
 class TestTastyClient(unittest.TestCase):
 
     def test_find_pizza_margherita_recipes(self):
         recipes = tasty.find_reciepes('pizza margherita')
-
-        for recipe in recipes:
-            print(recipe.key)
-            print(recipe.full_name)
-            print(recipe.url)
-            print()
         self.assertGreaterEqual(len(recipes), 6)
         
     def test_get_pizza_margherita_by_mario_batali_recipe(self):
         recipe = tasty.get_recipe('https://tasty.co/recipe/pizza-margherita-by-mario-batali')
 
-        print(recipe.title)
+        for ingredient in recipe.ingredients:
+            print(ingredient)
+
         self.assertEqual(recipe.title, 'Pizza Margherita by Mario Batali')
+        self.assertEqual(len(recipe.ingredients), 11)
+        self.assertEqual(recipe.ingredients[0], '1 ¼ cups water (300 mL), warm')
+        self.assertEqual(recipe.ingredients[1], '¼ oz active dry yeast (10 g), 1 packet')
+        self.assertEqual(recipe.ingredients[2], '1 ½ teaspoons sugar')
+        self.assertEqual(recipe.ingredients[3], '3 ½ cups all-purpose flour (435 g)')
+        self.assertEqual(recipe.ingredients[4], '2 tablespoons salt')
+        self.assertEqual(recipe.ingredients[5], '¼ cup extra virgin olive oil (60 mL)')
+        self.assertEqual(recipe.ingredients[6], '2 cups tomato (400 g), strained')
+        self.assertEqual(recipe.ingredients[7], 'fresh basil')
+        self.assertEqual(recipe.ingredients[8], '8 oz fresh mozzarella cheese (225 g)')
+        self.assertEqual(recipe.ingredients[9], 'olive oil')
+        self.assertEqual(recipe.ingredients[10], 'salt, to taste')
     
 if __name__ == '__main__':
     unittest.main()
